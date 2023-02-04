40226073ee750028c123002a0a2874625f3ad4df
Shashank Ramesh <73171829+iShshnk@users.noreply.github.com>
2023-02-04 13:51:05 +0530

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,8 +1,16 @@
 global facts
 global is_changed
-
 is_changed = True
-facts = [["vertebrate","duck"],["flying","duck"],["mammal","cat"]]
+facts = [["breathe","dog"],
+         ["eat","cow"],
+         ["2 legs","John"],
+         ["move","goat"],
+         ["skin","cat"],
+         ["skin","giraffe"],
+         ["fly","parrot"],
+         ["2 legs","kangaroo"],
+         ["intelligence","John"],
+         ["egg","hen"]]
 
 def assert_fact(fact):
     global facts
@@ -14,11 +22,25 @@ def assert_fact(fact):
 while is_changed:
     is_changed = False
     for A1 in facts:
-        if A1[0] == "mammal":
-            assert_fact(["vertebrate",A1[1]])
-        if A1[0] == "vertebrate":
-            assert_fact(["animal",A1[1]])
-        if A1[0] == "vertebrate" and ["flying",A1[1]] in facts:
-            assert_fact(["bird",A1[1]])
+        if A1[0] == "breathe":
+            assert_fact(["Living_thing",A1[1]])
+        if A1[0] == "eat":
+            assert_fact(["Living_thing",A1[1]])
+        if A1[0] == "Animal":
+            assert_fact(["Living_thing",A1[1]])
+        if A1[0] == "Bird":
+            assert_fact(["Living_thing",A1[1]])
+        if A1[0] == "Human":
+            assert_fact(["Living_thing",A1[1]])
+        if A1[0] == "move":
+            assert_fact(["Animal",A1[1]])
+        if A1[0] == "fly" or A1[0] == "egg":
+            assert_fact(["Bird",A1[1]])
+        if A1[0] == "skin" or A1[0] == "2 legs":
+            assert_fact(["Animal",A1[1]])
+        if A1[0] == "2 legs" and ["intelligence",A1[1]] in facts:
+            assert_fact(["Human",A1[1]])
 
-print(facts)
+for A1 in facts:
+    if A1[0] == "Living_thing":
+        print(A1)
